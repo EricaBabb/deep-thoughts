9b599be5cb01cdee150e289249c46bf30685f7cd
EricaBabb <ecamilebabb@gmail.com>
2021-09-03 18:05:30 -0400

    added routes and connected queries to front end

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,18 @@ import React from 'react';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+//renamed to Router b/c easier to work with
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 //uri = uniform resource identifier
 // const httpLink = createHttpLink({
@@ -33,15 +41,28 @@ function App() {
   return (
     //passing client as the prop to basically allow all the info enclosing ApolloProvider to reach the client (the httpLink website where we can view)
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        {/* wrapped div makes all of the child components on the page aware of the client-side routing that can take place now */}
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/signup" component={Signup} />
+    {/* ? means the parameter is optional */}
+    <Route exact path="/profile/:username?" component={Profile} />
+    <Route exact path="/thought/:id" component={SingleThought} /> 
+
+    <Route component={NoMatch} />
+  </Switch>
+</div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
+
 
 export default App;
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+
+  //extracting the id from single thought from the URL
+  const { id: thoughtId } = useParams();
+
+  //this is how you pass variables to queries that need them
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+  variables: { id: thoughtId }
+  //The id property on the variables object will become the $id parameter in the GraphQL query
+});
+
+  const thought = data?.thought || {};
+
+if (loading) {
+  return <div>Loading...</div>;
+}
+
   return (
     <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            Username
-          </span>{' '}
-          thought on createdAt
-        </p>
-        <div className="card-body">
-          <p>Thought Text</p>
-        </div>
-      </div>
+  <div className="card mb-3">
+    <p className="card-header">
+      <span style={{ fontWeight: 700 }} className="text-light">
+        {thought.username}
+      </span>{' '}
+      thought on {thought.createdAt}
+    </p>
+    <div className="card-body">
+      <p>{thought.thoughtText}</p>
     </div>
+  </div>
+
+  {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+</div>
   );
 };
 
