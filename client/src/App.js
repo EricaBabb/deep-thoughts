1f4802c89b86b5dd836124383f84a40634f784c3
EricaBabb <ecamilebabb@gmail.com>
2021-09-03 15:53:18 -0400

    added concurrently library as dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
 
+//ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
+
+//ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server
+
+//InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+
+//createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+//uri = uniform resource identifier
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:3001/graphql',
+// });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    //passing client as the prop to basically allow all the info enclosing ApolloProvider to reach the client (the httpLink website where we can view)
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import ThoughtList from '../components/ThoughtList';
+
+//useQuery Hook allows home to make requests to the GraphQL server
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
+  // use useQuery hook to make query request
+  //asynchronous = loading property to indicate that the request isn't done just yet 
+  //When finished and we have data returned from the server, info is stored in the destructured data property
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  //loading property allows ablility to conditionally render data based on whether or not there is data to even display
+
+  //every GraphQL response comes in a big data object, so we need to isolate thoughts data
+  //synthax is optional chaining
+  //Optional chaining negates the need to check if an object even exists before accessing its properties
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+  //if data exists, store it in the thoughts constant we just created. If data is undefined, then save an empty array to the thoughts component.
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
-      </div>
-    </main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+      )}
+    </div>
+  </div>
+</main>
   );
 };
 
